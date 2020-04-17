7a00a9d7d326ee37e30a8d3e2d44241d20638fc9
Aksh2209 <akshayap672@gmail.com>
2020-04-17 16:48:42 +0530

    login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'login-section',
-  template: `Pricing template`
+  template: `lo template`
 })
 export class LoginSection { }
