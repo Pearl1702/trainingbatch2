445bb14de4fa7da56706beaf0b594cb6acf95f5c
Aksh2209 <akshayap672@gmail.com>
2020-04-20 20:36:04 +0530

    Header-section modified

--- src/app/global/header/header.component.ts
+++ src/app/global/header/header.component.ts
@@ -4,12 +4,13 @@ import { Component } from '@angular/core';
   selector: 'header-section',
   template: `
   <nav class="navbar">
-      <img src="https://www.torryharris.com/images/this/torry-harris-integration-home.svg" placeholder="Logo">
-      <a href="#Home">Home</a>
-      <a href="#About">About</a>
-      <a href="#Contact Us">Contact Us</a>
-  </nav>`,
-  
+    <img class="logo" src="https://www.torryharris.com/images/this/torry-harris-integration-home.svg" placeholder="Logo">
+    <a href="#Home">Home</a>
+    <a href="About">About</a>
+    <a href="#Contact Us">Contact Us</a>
+  </nav>
+  `,
+  styles:['.navbar {background-color: rgb(255,69,0);} .logo {height: 4rem; width: 12rem; vertical-align: middle; }.navbar a { color: #d3e3e7; font-size: 20px; padding: 0 20px; ']
 })
 
 export class HeaderComponent {  }
