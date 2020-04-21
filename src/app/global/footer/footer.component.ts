7a4cd3d775a41a476a67807f8bc304d2d5a79b7b
Aksh2209 <akshayap672@gmail.com>
2020-04-21 17:26:29 +0530

    Little modifications in header and footer component

--- src/app/global/footer/footer.component.ts
+++ src/app/global/footer/footer.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'footer',
-  template:'<div class="footer"><p>copyright &copy; 2020</p></div>',
-  styles:['.footer{position: relative;text-align:center; bottom: 0width:100%;height: 2.5rem;} .footer p{color: rgb(128, 128, 128);}']
+  template:`
+  <div class="footer">
+    <p> &copy; Copyrights Torry Harris Bussiness Solution</p>
+  </div>`,
+  styles:['.footer{	position: relative; bottom: 0; width: 100%;}.footer p { font-size: 8pt; padding 2px 20px;}']
 })
 export class FooterComponent  { 
  
--- src/app/global/header/header.component.ts
+++ src/app/global/header/header.component.ts
@@ -4,13 +4,13 @@ import { Component } from '@angular/core';
   selector: 'header-section',
   template: `
   <nav class="navbar">
-    <img class="logo" src="https://www.torryharris.com/images/this/torry-harris-integration-home.svg" placeholder="Logo">
-    <a href="#Home">Home</a>
-    <a href="#About">About</a>
+    <img class="logo" src="https://www.torryharris.com/images/this/this_logo_innerpages.svg" placeholder="Logo">
     <a href="#Contact Us">Contact Us</a>
+    <a href="#About">About</a>
+    <a href="#Home">Home</a>
   </nav>
   `,
-  styles:['.navbar {background-color: rgb(255,69,0);} .logo {height: 3rem; width: 12rem; float: left;}.navbar a { color: #d3e3e7; font-size: 20px; float: left']
+  styles:['.navbar {background-color: rgb(255,255,255)}.logo {height: 3rem; width: 12rem;}.navbar a { color: rgba(0, 0, 0, .8); font-size: 20px; font-weight: bold; float: right']
 })
 
 export class HeaderComponent {  }
