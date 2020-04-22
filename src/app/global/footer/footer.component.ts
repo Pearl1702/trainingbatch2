1a1b496b2867585c3983bfa94d2e0b694765f11c
Aksh2209 <akshayap672@gmail.com>
2020-04-22 14:22:22 +0530

    Footer component modified

--- src/app/global/footer/footer.component.ts
+++ src/app/global/footer/footer.component.ts
@@ -4,8 +4,7 @@ import { Component } from '@angular/core';
   selector: 'footer',
   template:`
   <div class="footer">
-    <mat-card>@Copyrights Torry Harris Business Solution </mat-card>
-  </div>`,
-  styles:['.footer{	position: relative; bottom: 0; width: 100%; height: 10px;}.mat-card{background-color: rgb(255, 255, 255);text-align:left; font-size: 10pt;padding: 4px 10px}']
+    <p>@Copyrights Torry Harris Business Solution </p>
+  </div>`
 })
 export class FooterComponent { }
